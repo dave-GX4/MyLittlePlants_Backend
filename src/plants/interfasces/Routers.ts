d327422a3632026e579edc1ab1ba1b47f7cad6df
dave-GX4 <zombie3431@gmail.com>
2025-08-01 16:13:53 -0600

    Proteger rutas de plantas

--- src/plants/interfasces/Routers.ts
+++ src/plants/interfasces/Routers.ts
@@ -1,23 +1,49 @@
 import { Router } from 'express';
 import { addPlantController, deleteController, getAllController, getByIdController, searchPlantController, updateController } from './Dependecncias';
 import { Upload } from './middleware/Upload';
+import { isAuthenticated, hasRole } from '../../core/middlewares/authMiddleware';
 
 const plantRouter = Router();
 
-plantRouter.get('/plants/search', (req, res) => searchPlantController.run(req, res));
-plantRouter.delete('/plants/:id', (req, res) => deleteController.run(req, res));
-plantRouter.get('/plants', (req, res) => getAllController.run(req, res));
-plantRouter.get('/plants/:id', (req, res) => getByIdController.run(req, res));
+// Rutas protegidas para usuarios y vendedores
+plantRouter.get(
+    '/plants/search',
+    isAuthenticated,
+    (req, res) => searchPlantController.run(req, res)
+);
+plantRouter.get(
+    '/plants',
+    isAuthenticated,
+    (req, res) => getAllController.run(req, res)
+);
+plantRouter.get(
+    '/plants/:id', 
+    isAuthenticated,
+    (req, res) => getByIdController.run(req, res)
+);
 
+// Rutas protegigas solo para vendedor (eliminar planta)
+plantRouter.delete(
+    '/plants/:id', 
+    isAuthenticated,
+    hasRole(['Vendedor']),
+    (req, res) => deleteController.run(req, res)
+);
+
+// Rutas protegigas solo para vendedor (registro y actualizacion)
 plantRouter.post(
     '/plants', 
+    isAuthenticated,
+    hasRole(['Vendedor']),
     Upload.single('plantImage'),
     (req, res) => addPlantController.run(req, res)
 );
 
 plantRouter.put(
-    '/plants/:id', 
-    Upload.single('plantImage'), // <-- AÑADIDO AQUÍ
+    '/plants/:id',
+    isAuthenticated,
+    hasRole(['Vendedor']),
+    Upload.single('plantImage'),
     (req, res) => updateController.run(req, res)
 );
 
